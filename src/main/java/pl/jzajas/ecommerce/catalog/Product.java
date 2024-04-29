63151e7bc914f3210f4065b747c0908fc1d3cbfb
s-301-05 <s-301-05@gumiforest.uek.krakow.pl>
2024-04-29 16:32:13 +0200

    zajecia

--- src/main/java/pl/jzajas/ecommerce/catalog/Product.java
+++ src/main/java/pl/jzajas/ecommerce/catalog/Product.java
@@ -4,10 +4,14 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 public class Product {
-    private final String id;
-    private final String name;
-    private final String description;
+    private  String id;
+    private  String name;
+    private  String description;
     private BigDecimal price;
+
+    public Product() {
+
+    }
 
     public Product(UUID id, String name, String description) {
         this.id = id.toString();
